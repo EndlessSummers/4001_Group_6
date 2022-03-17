0a779c50c78dbcaaaec12566fb1ccb2446daa347
PalePhantom <hengyucao@126.com>
2022-03-17 16:08:21 +0800

    Add new functions

--- validation.js
+++ validation.js
@@ -60,7 +60,64 @@ function validate(component) {
             para.innerHTML = "some error happened";
             para.setAttribute('hidden');
         }
-    }
+    } else if (component === "email") {
+        let line = document.forms["register"]["email"].value;
+        let para = document.getElementById("email_e");
+		let reg = /^[a-zA-Z0-9][\w\-]+@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$/
+        if (!reg.test(line)) {
+            console.log("ERROR: EMAIL: INCORRECT EMAIL FORMAT");
+            para.innerHTML = "incorrect email format";
+            para.removeAttribute('hidden');
+        } else {
+            // validation passed
+            // do something here
+            console.log("VAILD EMAIL");
+            para.innerHTML = "some error happened";
+            para.setAttribute('hidden');
+        }
+    } else if (component === "newpassword") {
+        let line_0 = document.forms["register"]["oldpassword"].value;
+        let line_1 = document.forms["register"]["newpassword"].value;
+        let para = document.getElementById("newpassword_e");
+        if (line_0 == line_1) {
+            console.log("ERROR: PASSWORD: SAME");
+            para.innerHTML = "the new password cannot be the same as the old one";
+            para.removeAttribute('hidden');
+        } else if (line_1.length != line_1.trim().length) {
+            console.log("ERROR: PASSWORD: LEADING/TAILING SPACE");
+            para.innerHTML = "password should contain no leading or tailing spaces";
+            para.removeAttribute('hidden');
+        } else if (line_1.length < 8 || line_1.length > 16) {
+            console.log("ERROR: PASSWORD: LENGTH");
+            para.innerHTML = "password must contain 8-16 (included) characters";
+            para.removeAttribute('hidden');
+        } else if (!(/^.*[A-Z].*$/.test(line_1) && /^.*[a-z].*$/.test(line_1) && /^.*[0-9].*$/.test(line_1))) {
+            console.log("ERROR: PASSWORD: FORMAT");
+            para.innerHTML = "password must contain at least one lowercase character, one uppercase character, and one digit";
+            para.removeAttribute('hidden');
+        } else {
+            // validation passed
+            // do something here
+            console.log("DIFFERENT NEW PASSWORD");
+            para.innerHTML = "some error happened";
+            para.setAttribute('hidden');
+        }
+    } else if (component === "repeatnew") {
+        let line_0 = document.forms["register"]["newpassword"].value;
+        let line_1 = document.forms["register"]["repeatnew"].value;
+        let para = document.getElementById("repeatnew_e");
+        if (line_0 != line_1) {
+            console.log("ERROR: REPEAT: NOT MATCH");
+            para.innerHTML = "password did not match";
+            para.removeAttribute('hidden');
+        } else {
+            // validation passed
+            // do something here
+            console.log("DIFFERENT NEW PASSWORD ");
+            para.innerHTML = "some error happened";
+            para.setAttribute('hidden');
+        }
+    } 
 }
 
 function validateForm() {
