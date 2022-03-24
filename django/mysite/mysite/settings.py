595c12c14bf978daf36293335aeaaedfd62b0630
EndlessSummers <Yaphetsforever0514@outlook.com>
2022-03-24 23:11:33 +0800

    combine_phase_1

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Funbox.apps.FunboxConfig'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
