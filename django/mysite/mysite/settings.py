1d582fa7a81f0d854d50a605f9e7966bbcbf99bf
Peter <118010389@link.cuhk.edu.cn>
2022-04-11 01:04:59 +0800

    1
    1

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -129,3 +129,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 设置邮件域名 
+EMAIL_HOST = 'smtp.163.com'
+# 设置端口号，为数字
+EMAIL_PORT = 25
+#设置发件人邮箱
+EMAIL_HOST_USER = 'Funbox2022@163.com'
+# 设置发件人 授权码
+EMAIL_HOST_PASSWORD = 'WHNPGEDNZDZDLUUF'
+# 设置是否启用安全链接
+EMAIL_USER_TLS = True
+
+# 以上这个配置信息，Django会自动读取，
+# 使用账号以及授权码进行登录，
+# 如果登录成功，可以发送邮件
