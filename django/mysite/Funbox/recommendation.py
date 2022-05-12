16ab3d8ada39cc8536c348dc63a8944683ed44c0
Peter <118010389@link.cuhk.edu.cn>
2022-05-12 11:59:24 +0800

    Update recommendation.py
    comment added

--- django/mysite/Funbox/recommendation.py
+++ django/mysite/Funbox/recommendation.py
@@ -4,38 +4,51 @@ from Funbox.models import Activities, UserHash, UserInfo, UserPreference, Notes
 item_id_dict= dict() #dictionary for item id 和 列数 （id为1的item 在第一列）
 
 def load_data():
-    '''测试数据'''
-    #to do: 将sql数据写入
+    # test data format  1
     data_mat = np.mat([[1, 1, 1, 0, 0], [2, 2, 2, 0, 0], [1, 1, 1, 0, 0], [5, 5, 5, 0, 0],
                      [1, 1, 0, 2, 2], [0, 0, 0, 3, 3], [0, 0, 0, 1, 1]])
     return data_mat
 
-# 相似度计算...
+def loadExData2():
+    # test data format2
+    data= [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+           [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+           [0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+           [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+           [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+           [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+           [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1],
+           [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+           [0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1],
+           [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+           [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0]]       
+    return np.mat(data)
+# Similarity Calculation
 def eu_dis(inv1, inv2):
-    '''欧氏距离相似度'''
+    # Euclidean Correlation
     return 1.0/(1.0+np.linalg.norm(inv1-inv2))
 
 def pear_dis(inv1, inv2):
-    '''皮尔逊相关系数'''
+    #Pearson Correlation
     if len(inv1) < 3:
         return 1.0
     return 0.5 + 0.5 * np.corrcoef(inv1, inv2, rowvar=False)[0][1]
 
 def cos_dis(inv1, inv2):
-    '''余弦相似度'''
+    # Cosine Similarity
     num = float(inv1.T*inv2)
     den = np.linalg.norm(inv1) * np.linalg.norm(inv2)
     return num/den
 
 def stand_est(data_mat, user, sim_means, item):
-    '''计算在给定相似度计算方法的条件下，用户对物品的估计评分值'''
-    n = data_mat.shape[1]  # 取数据集的列数
+    # given any similarity calculation method, rate each item
+    n = data_mat.shape[1]  # num of cols
     sim_total = 0.0; rat_sim_total = 0.0
     for j in range(n):
-        user_rating = data_mat[user, j]  # 通过循环得到地user行的每列元素
+        user_rating = data_mat[user, j]  # retrieve every elements from each cols iterately
         if user_rating == 0:
             continue
-        over_lap = np.nonzero(np.logical_and(data_mat[:, item].A>0, data_mat[:, j].A>0))[0]
+        over_lap = np.nonzero(np.logical_and(data_mat[:, item].A>0, data_mat[:, j].A>0))[0] #for calculation of similarity
 
         if len(over_lap) == 0:
             sim_laity = 0.0
@@ -43,7 +56,7 @@ def stand_est(data_mat, user, sim_means, item):
             sim_laity = sim_means(data_mat[over_lap, item], data_mat[over_lap, j])
             
         sim_total += sim_laity
-        rat_sim_total += sim_laity * user_rating
+        rat_sim_total += sim_laity * user_rating #update similarity
     if sim_total == 0:
         return 0
     else:
@@ -64,9 +77,9 @@ def svd_est(data_mat, user, sim_means, item):
     #using SVD
     n = data_mat.shape[1]
     sim_total = 0.0; rate_sim_total = 0.0
-    u, sigma, v = np.linalg.svd(data_mat)
+    u, sigma, v = np.linalg.svd(data_mat) #SVD decomposition
     sig4 = np.mat(np.eye(4)*sigma[:4])
-    x_formed_items = data_mat.T * u[:, :4] * sig4.I
+    x_formed_items = data_mat.T * u[:, :4] * sig4.I # for similarity calculation
     for j in range(n):
         user_rating = data_mat[user, j]
         if user_rating == 0 or j == item:
@@ -74,45 +87,9 @@ def svd_est(data_mat, user, sim_means, item):
         sim_laity = sim_means(x_formed_items[item, :].T, x_formed_items[j, :].T)
         print('the %d and %d similarity is %f' % (item, j, sim_laity))
         sim_total += sim_laity
-        rate_sim_total += sim_laity * user_rating
+        rate_sim_total += sim_laity * user_rating #update
     if sim_total == 0:
         return 0
     else:
         return rate_sim_total/sim_total
 
-def loadExData2():
-    # To do: 将data导入 最好有评分
-
-    # data= [[0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 5],
-    #        [0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 3],
-    #        [0, 0, 0, 0, 4, 0, 0, 1, 0, 4, 0],
-    #        [3, 3, 4, 0, 0, 0, 0, 2, 2, 0, 0],
-    #        [5, 4, 5, 0, 0, 0, 0, 5, 5, 0, 0],
-    #        [0, 0, 0, 0, 5, 0, 1, 0, 0, 5, 0],
-    #        [4, 3, 4, 0, 0, 0, 0, 5, 5, 0, 1],
-    #        [0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 4],
-    #        [0, 0, 0, 2, 0, 2, 5, 0, 0, 1, 2],
-    #        [0, 0, 0, 0, 5, 0, 0, 0, 0, 4, 0],
-    #        [1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0]]
-
-    data= [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-           [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-           [0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
-           [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0],
-           [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0],
-           [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
-           [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1],
-           [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-           [0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1],
-           [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-           [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0]]       
-    return np.mat(data)
-
-
-
-# data = loadExData2()
-# data = np.mat(data)
-# user = 3
-# N = 3
-# ans = recommend(data, user, N, sim_means=cos_dis, est_method=stand_est)
-# print(ans)#结果
